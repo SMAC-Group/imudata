2f5100921b1d6293f00756b18150692be179a7f1
coatless <balamut2@illinois.edu>
2015-09-23 20:50:16 -0500

    Initial build

---
+++ R/cont_imu1.R
@@ -0,0 +1,17 @@
+#' @name cont_imu1
+#' @title Containment IMU Data from a XSens MTi-G sensor
+#' @description This data set contains gyroscope data
+#' @docType data
+#' @usage cont_imu1
+#' @format A \code{matrix} with 500,000 observations and 1 variable. The variable is as follows:
+#' \itemize{
+#'  \item{Gyro. X}{Gyroscope X-Axis}
+#' }
+#' @source Geodetic Engineering Laboratory (TOPO), Swiss Federal Institute of Technology Lausanne (EPFL)
+#' @author JJB
+#' @examples
+#' \dontrun{
+#' data(cont_imu1)
+#' summary(cont_imu1)
+#' }
+NULL
---
+++ R/imu6.R
@@ -0,0 +1,22 @@
+#' @name imu6
+#' @title IMU Data from a XSens MTi-G sensor
+#' @description This data set contains gyroscope and accelerometer data from a XSens MTi-G sensor.
+#' @docType data
+#' @usage imu6
+#' @format A \code{matrix} with 873,684 observations and 6 columns. The columns are defined as follows:
+#' \itemize{
+#'  \item{Gyro. X}{Gyroscope X-Axis}
+#'  \item{Gyro. Y}{Gyroscope Y-Axis}
+#'  \item{Gyro. Z}{Gyroscope Z-Axis}
+#'  \item{Accel. X}{Accelerometer X-Axis}
+#'  \item{Accel. Y}{Accelerometer Y-Axis}
+#'  \item{Accel. Z}{Accelerometer Z-Axis}
+#' }
+#' @source Geodetic Engineering Laboratory (TOPO), Swiss Federal Institute of Technology Lausanne (EPFL)
+#' @author JJB
+#' @examples
+#' \dontrun{
+#' data(imu6)
+#' summary(imu6)
+#' }
+NULL
