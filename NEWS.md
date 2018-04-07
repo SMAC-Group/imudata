018033f5f238fc96ee47b1cf0a5a00939f7b6dcd
Gaetan Bakalli <gaetan.bakalli@unige.ch>
2018-04-07 16:12:46 -0400

    update website

--- NEWS.md
+++ NEWS.md
@@ -2,5 +2,8 @@
 
 ## New
 
-ADD NEW DATASETS HERE
+* `kvh1750.acc` - Six samples collected from KVH1750 IMU at 100Hrz Axis: X,Y,Z - Type: Acce,  from a stationary sensor run for 3 hours
+* `kvh1750.gyro` - Six samples collected from KVH1750 IMU at 100Hrz Axis: X,Y,Z - Type: Accelerometer,  from a stationary sensor run for 3 hours
+* `mtig1khrz` - Six samples collected from MTI-G-710 IMU at 1000 Hz Axis: X,Y,Z - Type: Gyroscope & Accelerometer,  from a stationary sensor run for 10 minutes
+* `adis16405` - Six samples collected from ADIS 16405 IMU at 100Hrz Axis: X,Y,Z - Type: Gyroscope & Accelerometer,  from a stationary sensor run for 3 hours
 
--- R/data.R
+++ R/data.R
@@ -24,30 +24,6 @@
 "imu6"
 
 
-#' @title IMU Data from a XSens MTi-G sensor
-#' @description This data set contains gyroscope and accelerometer data from a XSens MTi-G sensor.
-#' @format An \code{imu} with 873,684 observations and 6 sensors. The sensors are defined as follows:
-#' \describe{
-#'  \item{\code{Gyro. X}}{Gyroscope X-Axis}
-#'  \item{\code{Gyro. Y}}{Gyroscope Y-Axis}
-#'  \item{\code{Gyro. Z}}{Gyroscope Z-Axis}
-#'  \item{\code{Accel. X}}{Accelerometer X-Axis}
-#'  \item{\code{Accel. Y}}{Accelerometer Y-Axis}
-#'  \item{\code{Accel. Z}}{Accelerometer Z-Axis}
-#' }
-#' @source Geodetic Engineering Laboratory (TOPO), Swiss Federal Institute of Technology Lausanne (EPFL)
-#' @author JJB
-#' @details 
-#' This data set is equivalent to the \code{\link[imudata]{imu6}} data set. The only difference
-#' is that this data set acts as a way to showcase the results of the \code{\link[gmwm]{imu}} cast function whereas 
-#' \code{\link[imudata]{imu6}} has yet to be cast using \code{\link[gmwm]{imu}}.
-#' @seealso \code{\link[imudata]{mtig}}
-#' @examples
-#' \dontrun{
-#' data(mtig)
-#' mtig
-#' }
-"mtig"
 
 #' @title IMU Data over 4 Hours from a navchip sensor
 #' @description This data set contains gyroscope and accelerometer data from navchip-based sensors.
